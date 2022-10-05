dcefc71b7c65890ced3e539acfe8920767235110
KungeniDjungeln <thhe05001@utb.vaxjo.se>
2022-10-05 12:11:52 +0200

    Add project files.

---
+++ Uppgift - 3.16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+ namespace Uppgift_3_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång är låten?");
+            int svar = int.Parse (Console.ReadLine());
+            if (svar >= 240 && svar <= 420)
+            {
+                Console.WriteLine("Du får spela låten");
+                
+
+            }
+            else
+            {
+                Console.WriteLine("Du får inte spela låten");
+            }
+        }
+    }
+}
+
+    
